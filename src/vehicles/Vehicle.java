eeddb788c441d750a67ffc5124e7dfd5d482b597
usuario <usuario@192.168.1.36>
2018-04-11 10:48:04 +0200

    commit 1 rama master

---
+++ src/vehicles/Vehicle.java
@@ -0,0 +1,20 @@
+package vehicles;
+
+public class Vehicle {
+
+		private int cilindrada;
+		private int portes;
+		private int velocitat_actual;
+		public static final int VELOCITAT_MAXIMA = 120;		
+		
+		public Vehicle(int cilindrada, int portes, int velocitat_actual) {
+			super();
+			this.cilindrada = cilindrada;
+			this.portes = portes;
+			this.velocitat_actual = velocitat_actual;
+		}
+		
+		public void accelerar(int velocitat) {				
+				velocitat += 10;
+		}
+}
